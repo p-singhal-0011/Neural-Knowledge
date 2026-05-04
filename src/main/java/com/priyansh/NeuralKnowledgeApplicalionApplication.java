bef8ed8262cfe98a991d03d6f422408ec1cb5bde
p-singhal-0011 <singhalpriyansh2005@gmail.com>
2026-05-04 12:56:01 +0530

    Initial clean commit

---
+++ src/main/java/com/priyansh/NeuralKnowledgeApplicalionApplication.java
@@ -0,0 +1,13 @@
+package com.priyansh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralKnowledgeApplicalionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuralKnowledgeApplicalionApplication.class, args);
+	}
+
+}
